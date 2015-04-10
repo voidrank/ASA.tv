c61fe578f887db30c04fb4d33a322e9ad51f9ffd
voidrank <voidrank@gmail.com>
2015-04-11 00:02:59 +0800

    commit

--- src/app/scripts/controllers/home.js
+++ src/app/scripts/controllers/home.js
@@ -1,5 +1,9 @@
 define('homeController', ['app', 'Uploader', 'UploadVideoCover', 'factories', 'less!homeStyle'], function(app, Upload, UploadVideoCover){
-  app.controller('home', function($scope, $http){
+
+  app
+
+
+  .controller('home', function($scope, $http){
     $scope.tab = [];
     for (var i = 0; i < 6; ++i)
       $scope.tab[i] = {};
@@ -9,8 +13,20 @@ define('homeController', ['app', 'Uploader', 'UploadVideoCover', 'factories', 'l
       $scope.tab[tabIndex].isActive = true;
     }
     $scope.changeTab(4);
+  })
+  
+
+  .controller('tab2', function($scope, $http){
+  })
+
+
+  .controller('tab4', ['$scope', '$http', 'collectionUrl', function($scope, $http, collectionUrl){
     $scope.videoCover = [];
     uploadVideoCover = new UploadVideoCover(document.getElementById('video-cover'), document.getElementById('video-cover-preview'));
-  });
+    $http.get(collectionUrl + 'is_member_of')
+    .success(function(res){
+      console.log(res);
+    })
+  }]);
 
 });
